c23bd486f64c4049a20f3301dec8160fc1d75b23
MuratHaktanir <79758626+MuratHaktanir@users.noreply.github.com>
2021-11-13 21:59:35 +0300

    Initial Commit

---
+++ Note/NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteApp.swift
+//  Note
+//
+//  Created by Murat Haktanır on 13.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
